6263848d978644788da28974930f1a11c1c48f1f
dogeonkim1 <caddibo4@naver.com>
2025-02-04 16:08:55 +0900

    lotto

--- src/lotto.ts
+++ src/lotto.ts
@@ -1,14 +1,14 @@
-document.querySelector("#btn")?.addEventListener("click",game)
+document.querySelector("#btn")?.addEventListener("click", game)
 function game():void{
-  //lotto번호를 위한 배열 선언
+  //lotto 번호를 위한 배열 선언 
   let lotto:number[]=[];
   while(lotto.length<6){
-    let num:number = parseInt(Math.random() * 45 +1+'');  //1 - 45사이의 난수 발생 시키기
-    //같은 수 배제하기
+    let num:number = parseInt(Math.random() * 45 +1+'');  //1 - 45 사이의 난수 발생 시키기 
+    //같은 수 배제하기 
     if(lotto.indexOf(num) == -1) lotto.push(num)
   }
 
-  lotto.sort(function(a, b){return a-b})
+  lotto.sort(function(a, b){ return a-b})
 
   let view:string='';
   let i:number = 0;
@@ -18,7 +18,8 @@ function game():void{
       clearInterval(intervalId)
       return
     }
-    view +=`<div class="ball ball${parseInt(lotto[i]/10+'')}">${lotto[i++]}</div>`
-    document.querySelector('#view')?.append(view)
+    view +=`<div class='ball ball${parseInt(lotto[i]/10+'')}'>${lotto[i++]}</div>`
+    let viewDiv = document.querySelector('#view')
+    viewDiv==null?'': viewDiv.innerHTML=view
   }, 1000)
 }
