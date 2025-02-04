9342a547cdbcea816dc50fb170684f9e13051e15
dogeonkim1 <caddibo4@naver.com>
2025-02-04 14:33:08 +0900

    union

--- src/07_union.ts
+++ src/07_union.ts
@@ -10,4 +10,20 @@ anyVal = 'hello';                                 //anyVal은 string 타입
 anyVal = false;                                   //anyVal은 boolean 타입
 // anyVal = {name:'hello'};                       //변수 선언시 지정한 타입이 아니므로 오류 발생
 
-let lateVal :string|undefined = undefined
+let lateVal :string|undefined|null = undefined
+lateVal = 'hello'
+
+/*
+  자주 사용하는 유니온 타입들을 별칭으로 지정해서 재사용하기
+  type 타입명 = 타입|타입|...
+*/
+type nsb = number|string|boolean
+
+let anyV:nsb = 10;
+anyV = 'hello';
+anyV = false;
+
+//타입 별칭 추가
+type nullNbs = nsb|null;
+
+let nullAbleValue: nullNbs = null;
