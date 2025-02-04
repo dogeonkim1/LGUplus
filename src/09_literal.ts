ff902f3a713c9d9b9f838e4d542474f7f11d41fc
dogeonkim1 <caddibo4@naver.com>
2025-02-04 14:46:43 +0900

    literal

---
+++ src/09_literal.ts
@@ -0,0 +1,8 @@
+/*
+literal
+변수의 값을 지정한 것중에 하나만 사용
+*/
+type cupSize = 'small'|'medium'|'large';
+
+let myCup:cupSize='small';
+// let otherCup:cupSize = 'tall';          //지정한 값이 아닌 다른 값을 대입하면 에러발생
