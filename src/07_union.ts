03374452fce2cd50efc394d3959670e7d08e8ebf
dogeonkim1 <caddibo4@naver.com>
2025-02-04 14:26:58 +0900

    union

--- src/07_union.ts
+++ src/07_union.ts
@@ -5,3 +5,9 @@ union
 변수명: 타입1|타입2|...
 */
 let anyVal : number|string|boolean = 10;          //anyVal은 number 타입
+
+anyVal = 'hello';                                 //anyVal은 string 타입
+anyVal = false;                                   //anyVal은 boolean 타입
+// anyVal = {name:'hello'};                       //변수 선언시 지정한 타입이 아니므로 오류 발생
+
+let lateVal :string|undefined = undefined
