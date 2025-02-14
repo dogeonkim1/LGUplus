b39c3e095a2345b02ae2e2b2e253cd3032610a53
dogeonkim1 <caddibo4@naver.com>
2025-02-14 10:39:50 +0900

    boj

--- baekjoon/0214_4101.js
+++ baekjoon/0214_4101.js
@@ -1,7 +1,16 @@
-//const today = new Date() -> 현재 날짜/시간을 불러옴 *해당 객체가 없으면 getFullYear, getMonth, getDate등을 사용할 수 없다.
+/*const today = new Date() -> 
+ 현재 날짜/시간을 불러옴 
+ *해당 객체가 없으면 getFullYear, getMonth, getDate등을 사용할 수 없다.*/
 
+//const year = today.getFullYear(); -> 현재 연도를 불러옴
 
+/*const month = String(today.getMonth()+1).padStart(2, '0'); 
+  -> 현재 월을 불러옴. 0월부터 시작하므로 getMonth()뒤에 +1 해줄것
+  -> .padStart(2,'0')해당 자릿수가 2자리가 아닐 경우 앞에 0을 붙여줌.(String에서만 사용 가능)
 
+*/
+
+//const day = String(today.getDate()).padStart(2, '0');
 
 
 const today = new Date()     
