a0eac14f4a86fc22a2b249bc6a9173b52b83571d
dogeonkim1 <caddibo4@naver.com>
2025-02-14 09:33:07 +0900

    2338

--- baekjoon/2338.js
+++ baekjoon/2338.js
@@ -1,10 +1,8 @@
 let fs = require('fs');
 let input = fs.readFileSync('input.txt').toString().trim().split('\n');
-input = input.map(Number)
+// input = input.map(Number)
 
 let A = BigInt(input[0]);
 let B = BigInt(input[1]);
 
-console.log(A + B);
-console.log(A - B);
-console.log(A * B);
+console.log(`${A + B}\n${A - B}\n${A * B}\n`);
