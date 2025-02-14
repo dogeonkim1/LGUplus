b510dcf16976edfd18a0e461c2e5dc392bedd37b
dogeonkim1 <caddibo4@naver.com>
2025-02-14 16:14:05 +0900

    알파벳 개수

--- baekjoon/0214_10808.js
+++ baekjoon/0214_10808.js
@@ -1,3 +1,9 @@
 const fs = require("fs");
-const input = fs.readFileSync("input.txt").toString().trim().split("\n")
+const input = fs.readFileSync("input.txt").toString().trim().split("");
 
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const counts = new Array(26).fill(0);
+
+input.forEach((i) => counts[alphabet.indexOf(i)]++);
+
+console.log(counts.join(" "));
