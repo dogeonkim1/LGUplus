5f90bc421cef53da7bafce3091ad7beb9d992f72
dogeonkim1 <caddibo4@naver.com>
2025-03-27 16:54:19 +0900

    boj:10818

--- baekjoon/10818.js
+++ baekjoon/10818.js
@@ -1,3 +1,11 @@
 const fs = require("fs");
 const input = fs.readFileSync("input.txt").toString().trim().split("\n");
 
+//숫자 배열로 변환 후 map에 저장
+const numbers = input[1].split(" ").map(Number);
+
+//최솟값과 최댓값 찾기
+const minNum = Math.min(...numbers);
+const maxNum = Math.max(...numbers);
+
+console.log(minNum, maxNum);
