9fc7d098fb8109f007ace3120048b85d0bdebd61
dogeonkim1 <caddibo4@naver.com>
2025-03-31 15:36:08 +0900

    boj: 25314

--- baekjoon/boj.js
+++ baekjoon/boj.js
@@ -1,10 +1,4 @@
-const fs = require("fs");
-const input = fs.readFileSync("input.txt").toString().trim().split("\n");
-
-let T = Number(input[0]);
-let numbers = input[1].split(" ").map(Number);
-let target = Number(input[2])
-
-let count = numbers.filter(num => num === target).length;
-
-console.log(count)
+let str = "2025-03-31"
+let arr = str.split('-');
+let year = arr.join('/')
+console.log(year)
